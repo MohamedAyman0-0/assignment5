dd6a6925e95d93006e71dd1a4b6633c478bde96a
mohamed ayman <mohamedaymanabdel3ty@gmail.com>
2025-08-04 20:14:19 +0300

    Add project files.

---
+++ assignment5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1. Difference Between Passing Value Type by Value vs. by Reference
+            #endregion
+            #region Q2.
+            #endregion
+            #region Q3.
+            #endregion
+            #region Q4.
+            #endregion
+            #region Q5.
+            #endregion
+            #region Q6.
+            #endregion
+            #region Q7.
+            #endregion
+            #region Q8.
+            #endregion
+        }
+    }
+}
