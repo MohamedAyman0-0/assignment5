2be9f16a6e4934bf2a57c6e0f608f984930fef76
mohamed ayman <mohamedaymanabdel3ty@gmail.com>
2025-08-04 20:19:08 +0300

    Q1. Difference Between Passing Value Type by Value vs. by Reference

--- assignment5/Program.cs
+++ assignment5/Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
             #region Q1. Difference Between Passing Value Type by Value vs. by Reference
+            static void ChangeValue(int x)
+            {
+                x = 100;
+            }
+
+            static void ChangeValueByRef(ref int x)
+            {
+                x = 100;
+            }
+
+            static void Main()
+            {
+                int a = 5;
+                ChangeValue(a);
+                Console.WriteLine("After by value: " + a);  // Output: 5
+
+                ChangeValueByRef(ref a);
+                Console.WriteLine("After by reference: " + a);  // Output: 100
+            }
             #endregion
             #region Q2.
             #endregion
